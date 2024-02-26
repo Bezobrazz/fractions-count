02a1cc8e3faa59d4d900cb5706def68ebe570c11
Bezobrazz <kostenko.uvd@gmail.com>
2024-02-26 21:56:29 +0200

    added code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [count3, setCount3] = useState(0);
-  const [count4, setCount4] = useState(0);
-  const [count5, setCount5] = useState(0);
+  const [counts, setCounts] = useState([0, 0, 0, 0, 0]);
   const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  const handleButtonClick = (count, setCount) => {
+  const handleButtonClick = (index) => {
     if (inputValue !== '') {
-      setCount(parseInt(inputValue) + count);
+      setCounts((prevCounts) => {
+        const newCounts = [...prevCounts];
+        newCounts[index] += parseInt(inputValue);
+        return newCounts;
+      });
       setInputValue('');
     }
   };
@@ -23,48 +23,20 @@ function App() {
   return (
     <>
       <input type="number" value={inputValue} onChange={handleInputChange} />
-      <button
-        onClick={() => handleButtonClick(count1, setCount1)}
-        type="button"
-        disabled={inputValue === ''}
-      >
-        1 Fraction
-      </button>
-      <button
-        onClick={() => handleButtonClick(count2, setCount2)}
-        type="button"
-        disabled={inputValue === ''}
-      >
-        2 Fraction
-      </button>
-      <button
-        onClick={() => handleButtonClick(count3, setCount3)}
-        type="button"
-        disabled={inputValue === ''}
-      >
-        3 Fraction
-      </button>
-      <button
-        onClick={() => handleButtonClick(count4, setCount4)}
-        type="button"
-        disabled={inputValue === ''}
-      >
-        4 Fraction
-      </button>
-      <button
-        onClick={() => handleButtonClick(count5, setCount5)}
-        type="button"
-        disabled={inputValue === ''}
-      >
-        5 Fraction
-      </button>
-
+      {counts.map((count, index) => (
+        <button
+          key={index}
+          onClick={() => handleButtonClick(index)}
+          type="button"
+          disabled={inputValue === ''}
+        >
+          {index + 1} Fraction
+        </button>
+      ))}
       <ul>
-        <li>1 fraction Sum: {count1}</li>
-        <li>2 fraction Sum: {count2}</li>
-        <li>3 fraction Sum: {count3}</li>
-        <li>4 fraction Sum: {count4}</li>
-        <li>5 fraction Sum: {count5}</li>
+        {counts.map((count, index) => (
+          <li key={index}>{index + 1} fraction Sum: {count}</li>
+        ))}
       </ul>
     </>
   );
